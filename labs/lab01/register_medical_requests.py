c181a612416eeb5aaba60004b5a88c0b8ade7717
Vasco Ramos <vascoalramos@outlook.pt>
2021-02-27 16:39:32 +0000

    Add basic functionality

--- labs/lab01/register_medical_requests.py
+++ labs/lab01/register_medical_requests.py
@@ -1,7 +1,49 @@
-from mysql.connector import connect, Error
+from mysql.connector import connect
+from hl7apy.core import Message
+
 from config import username, password, db_name
 
-conn = None
+
+def register_request(conn, r):
+    with conn.cursor() as cursor:
+        cursor.execute(
+            f"""INSERT INTO requests (date, hour, patient_id, patient_name, patient_address, patient_phone_number, episode_number, info)
+            VALUES ('{r["date"]}', '{r["hour"]}', {r["patient"]["number"]}, '{r["patient"]["name"]}', '{r["patient"]["address"]}', '{r["patient"]["phone_number"]}', {r["episode_number"]}, '{r["clinical_info"]}')"""
+        )
+        # TODO: insert to work list
+
+        conn.commit()
+        return cursor.lastrowid
+
+
+def cancel_request(conn):
+    print("Operation not yet implemented\n")
+
+
+def check_request_status(conn, req_id):
+    with conn.cursor() as cursor:
+        cursor.execute(f"SELECT status FROM requests WHERE number={req_id}")
+        results = cursor.fetchall()
+        if results:
+            return results[0][0]
+        else:
+            return None
+
+
+def get_request_report(conn, req_id):
+    with conn.cursor() as cursor:
+        cursor.execute(f"SELECT report FROM requests WHERE number={req_id}")
+        results = cursor.fetchall()
+        if results:
+            return results[0][0]
+        else:
+            return None
+
+
+def check_request_exists(conn, req_id):
+    with conn.cursor() as cursor:
+        cursor.execute(f"SELECT * FROM requests WHERE number={req_id}")
+        return cursor.fetchall() != []
 
 
 def menu():
@@ -35,16 +77,60 @@ def main():
             break
 
         elif op == "i":
-            print("Operation not yet implemented\n")
+            request = {"patient": {}}
+            try:
+                request["patient"]["number"] = int(input("Patient number: "))
+                request["patient"]["name"] = input("Patient name: ")
+                request["patient"]["address"] = input("Patient address: ")
+                request["patient"]["phone_number"] = input("Patient phone number: ")
+                request["date"] = input("Exam date: ")
+                request["hour"] = input("Exam hour: ")
+                request["episode_number"] = int(input("Episode number: "))
+                request["clinical_info"] = input("Aditional clinical info: ")
+            except:
+                print("Invalid input!")
+
+            try:
+                req_id = register_request(conn, request)
+                print(f"New request inserted successfully. ID: {req_id}\n")
+            except Exception as e:
+                print("Operation failded! Try again!\nError:" + str(e) + "\n")
 
         elif op == "c":
-            print("Operation not yet implemented\n")
+            cancel_request(conn)
 
         elif op == "s":
-            print("Operation not yet implemented\n")
+            try:
+                req_id = int(input("Medical exam request ID: "))
+
+                if check_request_exists(conn, req_id):
+                    status = check_request_status(conn, req_id)
+                    print(
+                        f"The status for the request with ID '{req_id}' is:  {status.upper()}\n"
+                    )
+                else:
+                    print(f"The request with ID '{req_id}' does not exist!\n")
+            except:
+                print("Invalid input!")
 
         elif op == "r":
-            print("Operation not yet implemented\n")
+            try:
+                req_id = int(input("Medical exam request ID: "))
+
+                if check_request_exists(conn, req_id):
+                    report = get_request_report(conn, req_id)
+                    if report:
+                        print(
+                            f"The report for the request with ID '{req_id}' is:\n{report}\n"
+                        )
+                    else:
+                        print(
+                            f"The request with ID '{req_id}' does not have a report yet!\n"
+                        )
+                else:
+                    print(f"The request with ID '{req_id}' does not exist!\n")
+            except:
+                print("Invalid input!")
 
         else:
             print("Invalid option!\n")
