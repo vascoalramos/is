e17093feffeffeb622f2a903256f440f41b329db
Vasco Ramos <vascoalramos@outlook.pt>
2021-04-17 09:51:59 +0100

    Update code

--- labs/lab02/service1/routes/interface/index.js
+++ labs/lab02/service1/routes/interface/index.js
@@ -1,9 +1,8 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 const patient = require("../../controllers/patient");
 
-/* GET home page. */
 router.get("/", function (req, res) {
     patient.list().then((data) => {
         res.render("index", { title: "Express", data: data });
