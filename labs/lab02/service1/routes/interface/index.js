c29b7639a3a0698fc04d48820e9405a03662e9c6
CarolinaRMarques <44268230+CarolinaRMarques@users.noreply.github.com>
2021-04-20 12:37:38 +0100

    added views

--- labs/lab02/service1/routes/interface/index.js
+++ labs/lab02/service1/routes/interface/index.js
@@ -5,8 +5,27 @@ const patient = require("../../controllers/patient");
 
 router.get("/", function (req, res) {
     patient.list().then((data) => {
-        res.render("index", { title: "Express", data: data });
+        res.render("index", { view : "addPacient", data: data });
     });
 });
+
+router.get("/addRequest", function (req, res) {
+    patient.list().then((data) => {
+        res.render("index", { view : "addRequest", data: data });
+    });
+});
+
+router.get("/filter", function (req, res) {
+    patient.list().then((data) => {
+        res.render("index", { view : "filter", data: data });
+    });
+});
+
+router.get("/list", function (req, res) {
+    patient.list().then((data) => {
+        res.render("index", { view : "listExams", data: data });
+    });
+});
+
 
 module.exports = router;
