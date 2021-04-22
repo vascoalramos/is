f37d794631cbe4f6d47e0264e38f529d0532c2f7
Vasco Ramos <vascoalramos@outlook.pt>
2021-04-22 20:20:11 +0100

    Add list table

--- labs/lab02/service1/routes/interface/index.js
+++ labs/lab02/service1/routes/interface/index.js
@@ -1,31 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-const patient = require("../../controllers/patient");
+const request = require("../../controllers/request");
 
 router.get("/", function (req, res) {
-    patient.list().then((data) => {
-        res.render("index", { view : "addPacient", data: data });
+    request.list().then((data) => {
+        console.log(data);
+        res.render("index", { view: "listExams", data: data });
     });
 });
 
 router.get("/addRequest", function (req, res) {
     patient.list().then((data) => {
-        res.render("index", { view : "addRequest", data: data });
+        res.render("index", { view: "addRequest", data: data });
     });
 });
 
 router.get("/filter", function (req, res) {
     patient.list().then((data) => {
-        res.render("index", { view : "filter", data: data });
+        res.render("index", { view: "filter", data: data });
     });
 });
 
-router.get("/list", function (req, res) {
+router.get("/addPacient", function (req, res) {
     patient.list().then((data) => {
-        res.render("index", { view : "listExams", data: data });
+        res.render("index", { view: "addPacient", data: data });
     });
 });
-
 
 module.exports = router;
